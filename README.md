f1b73d9c50e8cbb86bfe685530f4e29ec27e938b
Hirosaji <hiroya.kato@nex.nikkei.com>
2020-02-24 14:00:54 +0900

    create init dir for en

---
+++ README.md
@@ -0,0 +1,29 @@
+# bert-on-eb
+BERT on Elastic Beanstalk example
+
+original version: https://github.com/google-research/bert
+
+## What's example?
+To compute sentence similarity using BERT.
+
+## How to start this example?
+
+### Setup
+
+```
+$ pip install -r requirements.txt
+```
+
+### Get models
+
+TBA
+
+### Run on Local
+
+```
+$ python3 application.py
+```
+
+### Run on Elastic Beanstalk
+
+TBA
---
+++ application.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import os
+import sys
+
+sys.path.append("./bert_script")
+from params import BERT_PRAMS
+from extract_features import get_futures
+from util import is_japanese
+
+import numpy as np
+import codecs
+import tensorflow as tf
+import json
+
+
+def checkMount():
+    with open("/efs/sample_text.txt") as f:
+        output = f.read()
+    return output
+
+
+def cos_simularity(x, y):
+    return np.dot(x, y) / (np.linalg.norm(x) * np.linalg.norm(y))
+
+
+def calc_simlarity(total, each):
+    sims = [
+        cos_simularity(e["layers"][0]["values"], total["layers"][0]["values"])
+        for e in each
+    ]
+    return sims
+
+
+class convert_to_simlarity:
+    def __init__(self):
+        self.output = {"target": None, "texts": None, "sims": None}
+
+    def from_texts(self, target, texts):
+        self.output["target"] = target
+        self.output["texts"] = texts
+        self.output["sims"] = self.texts2similarity()
+
+        return self.output
+
+    def texts2similarity(self):
+        # get futures per sentence
+        body = [self.output["target"]] + self.output["texts"]
+        raw_features = get_futures(BERT_PRAMS, body)
+
+        # extract "[CLS]" features
+        cls_features = []
+        for raw_feature in raw_features:
+            cls_feature = list(
+                filter(lambda layer: layer["token"] == "[CLS]", raw_feature["features"])
+            )
+            cls_features.append(cls_feature[0])
+
+        # compute cosine simlarity
+        simlarities = calc_simlarity(cls_features[0], cls_features[1:])
+
+        return simlarities
+
+
+# EB looks for an 'application' callable by default.
+application = Flask(__name__)
+application.config["JSON_AS_ASCII"] = False
+
+# set convert_to_simlarity class
+req = convert_to_simlarity()
+
+# enable CORS
+CORS(application)
+
+# check post method behavior
+application.add_url_rule(
+    "/",
+    "index",
+    (
+        lambda: jsonify(
+            {
+                "type": "index",
+                "context": {"message": "Success!"}
+            }
+        )
+    ),
+    methods=["GET"],
+)
+
+# check whether there is mount file
+application.add_url_rule(
+    "/mount_check",
+    "mount check",
+    (
+        lambda: jsonify(
+            {
+                "type": "check mount file",
+                "context": {"mount_file": checkMount()}
+            }
+        )
+    ),
+    methods=["GET"],
+)
+
+# calculate sentence similarity
+application.add_url_rule(
+    "/sim",
+    "similarity",
+    (
+        lambda: jsonify(
+            {
+                "type": "sentence similarity",
+                "context": req.from_texts(
+                    request.get_json()["target"],
+                    request.get_json()["texts"]
+                )
+            }
+        )
+    ),
+    methods=["POST"],
+)
+
+# run the app.
+if __name__ == "__main__":
+    # Setting debug to True enables debug output. This line should be
+    # removed before deploying a production app.
+    application.debug = True
+    application.run()
